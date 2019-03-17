e999ec6fef3c549ae74e4ad1d5c3d7a574bc60ea
LostWeb <777kirill999@mail.ru>
2019-03-17 21:21:17 +0500

    new repos

---
+++ QuickSortingLab-master/QuickSorter.Test/QuickSorterUnitTests.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickSorter;
+
+namespace QuickSorter.Test
+{
+    [TestClass]
+    public class QuickSorterUnitTests
+    {
+        [TestMethod]
+        public void TestThreeElements()
+        {
+            var rnd = new Random(40);
+            var array = new int[3];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(100);
+            }
+
+            QuickSortClass.QuickSort(array);
+
+            Assert.IsTrue(array[0] < array[1] && array[1] <= array[2]);
+        }
+
+        [TestMethod]
+        public void Test100EqualsElements()
+        {
+            var array = new int[100];
+
+            QuickSortClass.QuickSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 5;
+            }
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(array[i], 5);
+            }
+        }
+
+        [TestMethod]
+        public void Test1000EqualsElements()
+        {
+            var rnd = new Random(40);
+            var array = new int[1000];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(100);
+            }
+
+            QuickSortClass.QuickSort(array);
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                var x = rnd.Next(100);
+                var y = rnd.Next(100);
+                while (x < y)
+                {
+                    x = rnd.Next(100);
+                    y = rnd.Next(100);
+                }
+                Assert.IsTrue(array[x] >= array[y]);
+            }
+        }
+
+        [TestMethod]
+        public void TestSortEmptyArray()
+        {
+            var array = new int[] { };
+
+            QuickSortClass.QuickSort(array);
+        }
+
+        [TestMethod]
+        public void Test1500000000Elements()
+        {
+            var rnd = new Random();
+            var array = new int[1500000000];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(100);
+            }
+
+            QuickSortClass.QuickSort(array);
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                Assert.IsTrue(array[i] <= array[i + 1]);
+            }
+        }
+    }
+}
---
+++ QuickSortingLab-master/QuickSorter/QuickSortClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSorter
+{
+    public class QuickSortClass
+    {
+        public static void QuickSort(int[] array)
+        {
+            int start = 0;
+            int end = array.Length - 1;
+            HoareSort(array, start, end);
+        }
+
+        static void Swap<W>(ref W firstElement, ref W secondElement)
+        {
+            W temp;
+            temp = firstElement;
+            firstElement = secondElement;
+            secondElement = temp;
+        }
+
+        static void HoareSort(int[] array, int startIndex, int endIndex)
+        {
+            if (endIndex == startIndex - 1) return;
+            if (endIndex == startIndex) return;
+            var pivot = array[endIndex];
+            var storeIndex = startIndex;
+            for (int i = startIndex; i <= endIndex - 1; i++)
+                if (array[i] <= pivot)
+                {
+                    Swap(ref array[i], ref array[storeIndex]);
+                    storeIndex++;
+                }
+            Swap(ref array[storeIndex], ref array[endIndex]);
+            if (storeIndex > startIndex)
+                HoareSort(array, startIndex, storeIndex - 1);
+            if (storeIndex < endIndex)
+                HoareSort(array, storeIndex + 1, endIndex);
+        }
+    }
+}
